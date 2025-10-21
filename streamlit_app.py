4414540f096f852399f7b591764d89e462dc9e20
GANDI13 <120286145+GANDI13@users.noreply.github.com>
2025-10-21 10:22:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model for synthetic_power_outage in Nigeria') 
 
-df = pd.read_csv('https://raw.githubusercontent.com/GANDI13/dp-machinelearning/refs/heads/master/synthetic_power_outage_data.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw DatA**')
+  df = pd.read_csv('https://raw.githubusercontent.com/GANDI13/dp-machinelearning/refs/heads/master/synthetic_power_outage_data.csv')
+  df
