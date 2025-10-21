5060a506471e95f44cb9514f3b786ef367f5f3c6
GANDI13 <120286145+GANDI13@users.noreply.github.com>
2025-10-21 09:46:31 +0100

    Fix typo in app information message

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🤖 Machine Learning App')
 
-st.info('This app builds a machine learning model for sythetic_power_outage in Nigeria') 
+st.info('This app builds a machine learning model for synthetic_power_outage in Nigeria') 
